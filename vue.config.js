ce798d475ec186fe273f278de2bd936d5ed7341d
Phi Long Le <philong.le@proton.me>
2025-05-03 15:32:51 +0200

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
-
-module.exports = {
+  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/Corsi/" : "/",
-};
-
+});
